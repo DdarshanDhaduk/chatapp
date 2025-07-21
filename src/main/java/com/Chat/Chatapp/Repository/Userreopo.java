dfbc0382ebbc862d2e23475e934a5ed5cb245b08
DarshanDhaduk <dhadukdarshan17@gmail.com>
2025-07-21 21:00:39 +0530

    init comit

--- src/main/java/com/Chat/Chatapp/Repository/Userreopo.java
+++
@@ -1,9 +0,0 @@
-package com.Chat.Chatapp.Repository;
-
-import com.Chat.Chatapp.Entity.Userentity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Userreopo extends JpaRepository<Userentity, String> {
-}
